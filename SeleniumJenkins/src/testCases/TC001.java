af42ca11e3421cedfca32045a48bc65a79f5ed80
priyaroop <priyaroop@josephs-mbp-3>
2020-03-08 20:01:48 -0400

    Changes to testCases

--- SeleniumJenkins/src/testCases/TC001.java
+++ SeleniumJenkins/src/testCases/TC001.java
@@ -5,20 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-
 public class TC001 {
-	
-	WebDriver driver;
-	
+
 	@Test
 	public void driverInstantiationAndClose() {
 		System.setProperty("webdriver.driver.chrome", "Driver/chromedriver");
-		driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
 		driver.findElement(By.name("email")).sendKeys("priyar");
 		driver.findElement(By.name("pass")).sendKeys("password");
-		driver.close();
-		
+		driver.quit();
+
 	}
 
 }
